dd7ccfa0f44c61b764e3608a7d8b8a52ab4eef35
vladgord64 <vladgord64@gmail/com>
2021-09-09 15:42:47 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
-import logo from './logo.svg';
+import bg1 from './assets/bg1.jpg';
+import bg2 from './assets/bg2.jpg';
 import './App.css';
 
-function App() {
+import Header from "./components/Header";
+import Layout from "./components/Layout";
+import Footer from "./components/Footer";
+
+import './index.css';
+
+
+const App =() => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World, React JS!
-        </p>
-      </header>
-    </div>
+    <>
+      <Header title="Tis is Title!!!!" descr="This is Descriptions." />
+      <Layout  title="This is Title of First Block" descr="Lorem lorem lorem lorem" urlBg={bg1}  colorBg={false} />
+      <Layout  title="This is Title of Second Block" descr="Lorem lorem lorem lorem Totem Uk"  urlBg=""  colorBg={true} />
+      <Layout  title="This is Title of Third Block" descr="Lorem lorem 4to escho echo php"  urlBg={bg2}  colorBg={false} />
+      <Footer />
+    </>
   );
 }
 
