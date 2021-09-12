e2c42be5be5e0c55655587f20b025ffd28f8b6f5
vladgord64 <90203534+vladgord64@users.noreply.github.com>
2021-09-12 23:00:09 +0300

    bg3

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import bg1 from './assets/bg1.jpg';
 import bg2 from './assets/bg2.jpg';
-import bg3 from './assets/bg3.jpg';
+//import bg3 from './assets/bg3.jpg';
 import './App.css';
 
 import Header from "./components/Header";
